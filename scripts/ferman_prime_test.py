d0a440abf3bc7a7cc92d8347a5c3f0f71a2105f4
blaza168 <jan.blazek10@gmail.com>
2020-10-21 11:26:27 +0200

    Update command line interface for ferman_prime_test.py and miller_rabin.py

--- scripts/ferman_prime_test.py
+++ scripts/ferman_prime_test.py
@@ -1,6 +1,6 @@
 from utils.multiply import square_multiply
-import sys
 import random
+from argparse import ArgumentParser
 
 
 def test(a, n):
@@ -8,12 +8,24 @@ def test(a, n):
 
 
 if __name__ == '__main__':
-    n = int(sys.argv[1])
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help='Display positives')
+    parser.add_argument('number', type=int)
+    args = parser.parse_args()
 
+    verbose = args.verbose
+    n = args.number
+
+    if verbose:
+        print("Positives: ", end='')
     for i in range(10):
         a = random.randint(1, n - 1)
         if not test(a, n):
             print("False")
             exit()
+        if verbose:
+            print(str(a) + " ", end='')
 
+    if verbose:
+        print()
     print("True")
--- scripts/miller_rabin.py
+++ scripts/miller_rabin.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 from utils.multiply import square_multiply
 import sys
 import random
@@ -31,15 +33,31 @@ def test(a, n):
 
 
 if __name__ == '__main__':
-    n = int(sys.argv[1])
-    iterations = 10
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help='Display positives')
+    parser.add_argument('-i', '--iterations', default=10, type=int, help='Number of iterations')
+    parser.add_argument('number', type=int)
+    args = parser.parse_args()
+
+    verbose = args.verbose
+    n = args.number
+    iterations = args.iterations
 
     chance = (1 - math.pow(0.25, iterations)) * 100
-    for i in range(10):
+    if verbose:
+        print("Positives: ", end='')
+
+    for i in range(iterations):
         a = random.randint(1, n - 1)
         if not test(a, n):
+            if verbose:
+                print()
             print("False")
             exit()
-    
+        if verbose:
+            print(str(a) + " ", end='')
+
+    if verbose:
+        print()
     print("Change: " + str(chance))
     print("True")
